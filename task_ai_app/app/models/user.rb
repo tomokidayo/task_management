452191e040ddeece307a2b5bdc51207aec25f7cf
tomokidayo <toblue0905@gmail.com>
2026-04-21 19:54:21 +0900

    バリデーション表示

--- task_ai_app/app/models/user.rb
+++ task_ai_app/app/models/user.rb
@@ -5,5 +5,17 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable
+
+  validates :name, presence: { message: "ユーザー名を入力してください" }
+  validates :email, presence: { message: "メールアドレスを入力してください" }
+  validates :password, length: { minimum: 6, message: "パスワードは6文字以上で入力してください" }, if: :password_required?
+
+  validates :email, uniqueness: { case_sensitive: false, message: "このメールアドレスは既に使用されています" }
+
+  private
+
+  def password_required?
+    new_record? || password.present? || password_confirmation.present?
+  end
 end
