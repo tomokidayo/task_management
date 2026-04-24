57c96283cd4bc4cb77bcd39de038add9d7b247d8
tomokidayo <toblue0905@gmail.com>
2026-04-24 21:13:29 +0900

    パスワード再設定修正

--- task_ai_app/app/controllers/mypages_controller.rb
+++ task_ai_app/app/controllers/mypages_controller.rb
@@ -11,16 +11,29 @@ class MypagesController < ApplicationController
 
   def update
     @user = current_user
-    if @user.update(user_params)
-      redirect_to mypage_path, notice: "更新しました"
+
+    # パスワード未入力 → パスワード以外を更新
+    if user_params[:password].blank?
+      if @user.update(user_params.except(:password, :password_confirmation))
+        redirect_to mypage_path, notice: "更新しました"
+      else
+        render :edit, status: :unprocessable_entity
+      end
+
+    # パスワード入力あり → パスワードも更新
     else
-      render :edit, status: :unprocessable_entity
+      if @user.update(user_params)
+        redirect_to mypage_path, notice: "パスワードを更新しました"
+      else
+        render :edit, status: :unprocessable_entity
+      end
     end
   end
+
 
   private
 
   def user_params
-    params.require(:user).permit(:name, :email, :password)
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
   end
 end
